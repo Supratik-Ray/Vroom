7fffd3ffb1e8aa2826a420b5a81d10db3bd72513
Supratik Ray <raysupratik8@gmail.com>
2026-04-02 18:14:15 +0530

    added cors in server

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,27 @@
 import express, { type Request, type Response } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+//http request handling
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.get("/", (req: Request, res: Response) => {
   console.log("working");
 });
 
+//socket.io connection
 io.on("connection", (socket) => {
   console.log(socket.id);
 });
